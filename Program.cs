10c729610f6a0a7db9cc66cf550205f6153d94e1
wooldridge12 <wooldridgemikey@gmail.com>
2021-10-28 13:53:29 -0400

    first push ot github

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// using System;
+
+// namespace Intro
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+// System.Console.WriteLine("Hello World");
+
+System.Console.Write("Who would you like to say hello to? ");
+
+string name = System.Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    System.Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    System.Console.WriteLine($"Hello, {name}!");
+}
